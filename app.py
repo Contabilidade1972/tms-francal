0689bce6082d0041ddda360e2713436d493d5428
Contabilidade1972 <lucianojorgesacramento@gmail.com>
2026-06-22 01:07:42 -0300

    Update app.py

--- app.py
+++ app.py
@@ -2,21 +2,21 @@ import streamlit as st
 import gspread
 import json
 
-st.title("TMS FRANCAL - ACESSO")
-
 def conectar():
-    # Carrega a string JSON do Secrets
+    # Carrega o JSON
     json_str = st.secrets["GCP_JSON"]
-    # Converte para dicionário Python
     creds_dict = json.loads(json_str)
     
-    # Conecta ao Google Sheets
+    # FORÇA a quebra de linha correta na chave privada
+    creds_dict["private_key"] = creds_dict["private_key"].replace('\\n', '\n')
+    
     gc = gspread.service_account_from_dict(creds_dict)
     return gc.open_by_key("1RFiXPxCLPTMBdGWVtPohslcSfgB8ef1_ZyU0IA9_Fgg").worksheet("REGISTRO_OPERACIONAL")
 
-if st.button("INICIAR CONEXÃO"):
+st.title("TMS FRANCAL - ACESSO")
+if st.button("CONECTAR"):
     try:
         ws = conectar()
-        st.success("Conectado com sucesso!")
+        st.success("Conectado!")
     except Exception as e:
-        st.error(f"Erro de conexão: {e}")
+        st.error(f"Erro: {e}")
